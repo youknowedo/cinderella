b6088679a438e65de70cd1893f9fb4e2f360a40a
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-03 00:15:36 +0200

    playlist selection

---
+++ src/lib/components/Combobox.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { cn } from '$lib/utils.js';
+	import Check from 'lucide-svelte/icons/check';
+	import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
+	import { tick } from 'svelte';
+
+	export let items: { label: string; value: string }[] = [];
+	export let itemName = 'item';
+	export let disabled = false;
+
+	let open = false;
+	export let value: string | null = null;
+
+	$: selectedLabel = items.find((f) => f.value === value)?.label ?? `Select ${itemName}...`;
+
+	// We want to refocus the trigger button when the user selects
+	// an item from the list so users can continue navigating the
+	// rest of the form with the keyboard.
+	function closeAndFocusTrigger(triggerId: string) {
+		open = false;
+		tick().then(() => {
+			document.getElementById(triggerId)?.focus();
+		});
+	}
+</script>
+
+<Popover.Root bind:open let:ids>
+	<Popover.Trigger asChild let:builder>
+		<Button
+			{disabled}
+			builders={[builder]}
+			variant="outline"
+			role="combobox"
+			aria-expanded={open}
+			class="w-[200px] justify-between"
+		>
+			{selectedLabel}
+			<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+		</Button>
+	</Popover.Trigger>
+	<Popover.Content class="w-[200px] p-0">
+		<Command.Root>
+			<Command.Input placeholder="Search {itemName}..." />
+			<Command.Empty>No {itemName} found.</Command.Empty>
+			<Command.Group>
+				{#each items as item}
+					<Command.Item
+						value={item.value}
+						onSelect={(currentValue) => {
+							value = currentValue;
+							closeAndFocusTrigger(ids.trigger);
+						}}
+					>
+						<Check class={cn('mr-2 h-4 w-4', value !== item.value && 'text-transparent')} />
+						{item.label}
+					</Command.Item>
+				{/each}
+			</Command.Group>
+		</Command.Root>
+	</Popover.Content>
+</Popover.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,71 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { PUBLIC_SPOTIFY_CLIENT_ID, PUBLIC_SPOTIFY_REDIRECT_URI } from '$env/static/public';
+	import Combobox from '$lib/components/Combobox.svelte';
 	import { Button } from '$lib/components/ui/button';
-	import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+	import {
+		SpotifyApi,
+		type Page,
+		type Playlist,
+		type TrackItem,
+		type User
+	} from '@spotify/web-api-ts-sdk';
 	import { onMount } from 'svelte';
 
 	let sdk: SpotifyApi | null = null;
+	let user: User | null = null;
+	let playlists: Playlist<TrackItem>[] | null = null;
+	let loadingPlaylists = false;
+	let selectedPlaylist: string | null = null;
 
 	onMount(() => {
 		sdk = SpotifyApi.withUserAuthorization(PUBLIC_SPOTIFY_CLIENT_ID, PUBLIC_SPOTIFY_REDIRECT_URI, [
+			'user-read-email',
 			'playlist-modify-public',
 			'playlist-modify-private'
 		]);
 	});
+
+	const getPlaylists = async () => {
+		if (!sdk) return;
+
+		if (!user) user = await sdk.currentUser.profile();
+
+		loadingPlaylists = true;
+
+		playlists = [];
+		let offset = 0;
+
+		let playlistPage = await sdk.playlists.getUsersPlaylists(user.id, 50);
+
+		while (playlistPage.items.length > 0) {
+			playlists = playlists.concat(playlistPage.items);
+			offset += playlistPage.items.length;
+			playlistPage = await sdk.playlists.getUsersPlaylists(user.id, 50, offset);
+		}
+
+		loadingPlaylists = false;
+	};
 </script>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center">
 	<h1>Cinderella</h1>
 	<p>Turn your random ordered playlist into something that makes sense.</p>
 
-	<Button
-		on:click={async () => {
-			sdk = SpotifyApi.withUserAuthorization(
-				PUBLIC_SPOTIFY_CLIENT_ID,
-				PUBLIC_SPOTIFY_REDIRECT_URI,
-				['playlist-modify-public', 'playlist-modify-private']
-			);
-			const user = await sdk.currentUser.profile();
-			console.log(user);
-		}}
-	>
-		Get Playlists
-	</Button>
+	<div>
+		<div class="flex gap-2">
+			<Combobox
+				disabled={!playlists}
+				bind:value={selectedPlaylist}
+				items={playlists ? playlists.map((p) => ({ label: p.name, value: p.id })) : []}
+				itemName="playlist"
+			/>
+			<Button disabled={!selectedPlaylist} on:click={() => goto(`/${selectedPlaylist}`)}
+				>Drop the shoe</Button
+			>
+		</div>
+		<Button disabled={loadingPlaylists} on:click={getPlaylists} variant="link" size="sm">
+			{loadingPlaylists ? 'Getting playlists...' : 'Get playlists'}
+		</Button>
+	</div>
 </div>
