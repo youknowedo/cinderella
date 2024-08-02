615eb23c2daca2c6b974b0da2e3a50cc2511ddae
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-02 23:17:05 +0200

    cant spotify auth to work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { PUBLIC_SPOTIFY_CLIENT_ID, PUBLIC_SPOTIFY_REDIRECT_URI } from '$env/static/public';
+	import { Button } from '$lib/components/ui/button';
+	import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+	import { onMount } from 'svelte';
+
+	let sdk: SpotifyApi | null = null;
+
+	onMount(() => {
+		sdk = SpotifyApi.withUserAuthorization(PUBLIC_SPOTIFY_CLIENT_ID, PUBLIC_SPOTIFY_REDIRECT_URI, [
+			'playlist-modify-public',
+			'playlist-modify-private'
+		]);
+	});
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<h1>Cinderella</h1>
+	<p>Turn your random ordered playlist into something that makes sense.</p>
+
+	<Button
+		on:click={async () => {
+			sdk = SpotifyApi.withUserAuthorization(
+				PUBLIC_SPOTIFY_CLIENT_ID,
+				PUBLIC_SPOTIFY_REDIRECT_URI,
+				['playlist-modify-public', 'playlist-modify-private']
+			);
+			const user = await sdk.currentUser.profile();
+			console.log(user);
+		}}
+	>
+		Get Playlists
+	</Button>
+</div>
