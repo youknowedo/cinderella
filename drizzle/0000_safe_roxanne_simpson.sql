f46924ac9d0787ce11b83306fb83a6e1385f03d8
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-04 02:23:59 +0200

    Revert "drizzle continuation"
    This reverts commit e6414e7e9b851270f0e0daf3d7f91cc60f3172cf.

--- drizzle/0000_safe_roxanne_simpson.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA "cinderella";
---> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "song" (
-	"id" text PRIMARY KEY NOT NULL,
-	"tempo" numeric
-);
--- src/lib/schema.ts
+++
@@ -1,8 +0,0 @@
-import { decimal, pgSchema, pgTable, text } from 'drizzle-orm/pg-core';
-
-export const schema = pgSchema('cinderella');
-
-export const song = pgTable('song', {
-	id: text('id').primaryKey(),
-	tempo: decimal('tempo')
-});
